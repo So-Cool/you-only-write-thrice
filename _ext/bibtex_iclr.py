757f8e530ed97ec666e9b5265b0246c495411086
Kacper Sokol <dev@kcpr.me>
2022-10-14 15:31:41 +0200

    Bibliography style update

--- _ext/bibtex_iclr.py
+++ _ext/bibtex_iclr.py
@@ -1,18 +1,25 @@
 # Copyright (C) 2021
-# Author: Kacper Sokol <ks1591@my.bristol.ac.uk>
+# Author: Kacper Sokol <kacper@xmlx.io>
 # License: new BSD
 """
 Implements the `iclr` BibTeX style for Jupyter Book and Sphinx
 (`sphinxcontrib-bibtex`).
 """
 
+__version__ = '0.2'
+
 import pybtex.plugin
+import sphinxcontrib.bibtex.plugin
+
+from dataclasses import dataclass, field
 from pybtex.style.formatting.alpha import Style
 from pybtex.style.labels import BaseLabelStyle as LabelStyle
+from sphinxcontrib.bibtex.style.referencing import BracketStyle
 from sphinxcontrib.bibtex.style.referencing.author_year import AuthorYearReferenceStyle
-from sphinxcontrib.bibtex.style.referencing import BaseReferenceText
+from sphinxcontrib.bibtex.style.template import SphinxReferenceInfo
 
 AYRS = AuthorYearReferenceStyle()
+REF_INFO = SphinxReferenceInfo('', '', '', '', '')
 
 class ICLR(Style):
     """Defines the `iclr` style for `sphinxcontrib-bibtex`."""
@@ -22,21 +29,35 @@ class ICLRlabel(LabelStyle):
     """Defines the `iclr-label` labelling style for `sphinxcontrib-bibtex`."""
     def format_labels(self, sorted_entries):
         for entry in sorted_entries:
-            ref = AYRS.inner('p').format_data(
-                data=dict(
-                    entry=entry,
-                    reference_info='',
-                    reference_text_class=BaseReferenceText,
-                    style=AYRS)
-            ).render_as('text')
+            data = dict(
+                entry=entry,
+                reference_info=REF_INFO,
+                style=AYRS
+            )
+            ref = str(AYRS.inner('p').format_data(data=data))
             yield ref
+
+def bracket_style() -> BracketStyle:
+    return BracketStyle(sep='; ')
+
+@dataclass
+class AuthorYearSemicolonReferenceStyle(AuthorYearReferenceStyle):
+    bracket_parenthetical: BracketStyle = field(default_factory=bracket_style)
+    bracket_textual: BracketStyle = field(default_factory=bracket_style)
+    bracket_author: BracketStyle = field(default_factory=bracket_style)
+    bracket_label: BracketStyle = field(default_factory=bracket_style)
+    bracket_year: BracketStyle = field(default_factory=bracket_style)
 
 def setup(app):
     """Sets up the `iclr` style for use with `sphinxcontrib-bibtex`."""
     if 'bibtex_reference_style' not in app.config.overrides:
-        app.config.overrides['bibtex_reference_style'] = 'author_year'
+        app.config.overrides['bibtex_reference_style'] = 'author_year_semicolon'
+    sphinxcontrib.bibtex.plugin.register_plugin(
+        'sphinxcontrib.bibtex.style.referencing',
+        'author_year_semicolon',
+        AuthorYearSemicolonReferenceStyle)
     pybtex.plugin.register_plugin(
         'pybtex.style.labels', 'iclr-label', ICLRlabel)
     pybtex.plugin.register_plugin(
         'pybtex.style.formatting', 'iclr', ICLR)
-    return {'version': '0.1'}
+    return {'version': __version__}
